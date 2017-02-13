ebcbf155c58dc990a64c62fc243244402a539a4b
tmucha <tmucha@cyfrowypolsat.pl>
2017-02-13 20:56:19 +0100

    new component of first app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
